613e81858475a67ed33d8baaedf05b78e8081207
DESKTOP\User <ab.1303@gmail.com>
2019-08-21 23:07:26 +1000

    Section: Component & DataBinding

--- ng2CompleteGuide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng2CompleteGuide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { createOutput } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,6 +9,9 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+
   recipes: Recipe[] = [
     new Recipe('A test Recipe', 'Simple test', 'https://img.bestrecipes.com.au/rZFo7F8i/h300-w400-cscale-1495077669/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg'),
     new Recipe('A pizza Recipe', 'Pizza recipe', 'https://img.bestrecipes.com.au/rZFo7F8i/h300-w400-cscale-1495077669/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg'),
@@ -16,6 +20,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeClicked(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
